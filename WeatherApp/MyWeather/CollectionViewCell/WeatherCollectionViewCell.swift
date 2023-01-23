08a0f616ece4af38c9972dbdb7a4e240bf2e30bb
lali <lalichubinidze.1999@gmail.com>
2023-01-23 14:22:52 +0400

    WeatherApp

---
+++ WeatherApp/MyWeather/CollectionViewCell/WeatherCollectionViewCell.swift
@@ -0,0 +1,27 @@
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "WeatherCollectionViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "WeatherCollectionViewCell",
+                     bundle: nil)
+    }
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+
+    func configure(with model: HourlyWeatherEntry) {
+        self.tempLabel.text = "\(model.temperature)"
+        self.iconImageView.contentMode = .scaleAspectFit
+        self.iconImageView.image = UIImage(named: "clear")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
+
